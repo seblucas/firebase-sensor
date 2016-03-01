63d3ad7c9c037e50e5c45f651971f70770f1f59f
Sébastien Lucas <sebastien@slucas.fr>
2016-03-01 08:38:57 +0100

    Add the router

--- app/app.module.js
+++ app/app.module.js
@@ -2,4 +2,13 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('sensorReadingApp', ['firebase', 'nvd3', 'templates']);
+angular.module('sensorReadingApp', ['firebase', 'nvd3', 'templates', 'ngRoute'])
+.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    template: '<home-page rooms="vm.rooms" errors="vm.errors"></home-page>'
+  })
+  .when('/errors', {
+    template: '<home-page rooms="vm.rooms" errors="vm.errors"></home-page>'
+  });
+});
