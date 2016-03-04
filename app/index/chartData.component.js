463ee13562c072a970646bde9debdc83e0484be7
Sébastien Lucas <sebastien@slucas.fr>
2016-03-04 10:02:02 +0100

    The chart is now a component. Way easier to read

---
+++ app/index/chartData.component.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+function ChartDataCtrl(lineChartService) {
+  var ctrl = this;
+  ctrl.chartData = [];
+  lineChartService.getChartData(ctrl.rooms, ctrl.type, ctrl.size).then(function(data) {
+    ctrl.chartData = data;
+  });
+
+  ctrl.chartOptions = lineChartService.getChartOption(ctrl.type);
+
+}
+
+angular.module('sensorReadingApp').
+component('chartData', {
+  bindings: {
+    rooms: '<',
+    size: '<',
+    type: '<'
+  },
+  templateUrl: 'index/chartData.html',
+  controller: ChartDataCtrl
+});
--- app/index/homePage.component.js
+++ app/index/homePage.component.js
@@ -1,32 +1,18 @@
 ﻿'use strict';
 
-function HomePageCtrl(lineChartService) {
+function HomePageCtrl() {
   var ctrl = this;
   ctrl.chartAllSizes = [{id: '24', value: '24 hours'},
                           {id: '48', value: '48 hours'},
                           {id: '168', value: '1 week'}];
   ctrl.chartSize = {id: '24'};
-  ctrl.tempOptions = lineChartService.getChartOption('temp');
-  ctrl.humOptions = lineChartService.getChartOption('hum');
-
-  ctrl.loadGraphs = function() {
-    ctrl.temperatures = [];
-    ctrl.humidities = [];
-    lineChartService.getChartData(ctrl.rooms, 'hum', ctrl.chartSize.id).then(function(data) {
-      ctrl.humidities = data;
-    });
-    lineChartService.getChartData(ctrl.rooms, 'temp', ctrl.chartSize.id).then(function(data) {
-      ctrl.temperatures = data;
-    });
-  };
-
-  ctrl.loadGraphs();
+  ctrl.dataTypes = [{type: 'temp'}, {type: 'hum'}];
 }
 
 angular.module('sensorReadingApp').
 component('homePage', {
   bindings: {
-    rooms: '=',
+    rooms: '<',
     errors: '='
   },
   templateUrl: 'index/homePage.html',
