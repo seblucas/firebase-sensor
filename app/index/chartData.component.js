70d14d1eeb9a85864758436f3fd29b140c79eba5
Sébastien Lucas <sebastien@slucas.fr>
2016-04-22 16:42:25 +0200

    The charts can now be refreshed

--- app/index/chartData.component.js
+++ app/index/chartData.component.js
@@ -2,13 +2,16 @@
 
 function ChartDataCtrl(lineChartService) {
   var ctrl = this;
-  ctrl.chartData = [];
-  lineChartService.getChartData(ctrl.rooms, ctrl.type, ctrl.size).then(function(data) {
-    ctrl.chartData = data;
-  });
+
+  ctrl.loadGraphs = function() {
+    ctrl.chartData = [];
+    lineChartService.getChartData(ctrl.rooms, ctrl.type, ctrl.size).then(function(data) {
+      ctrl.chartData = data;
+    });
+  };
 
   ctrl.chartOptions = lineChartService.getChartOption(ctrl.type);
-
+  ctrl.loadGraphs();
 }
 
 angular.module('sensorReadingApp').
