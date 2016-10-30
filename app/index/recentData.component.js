48c4d5703cf1362d8005832bf1378b4f0e4dfb05
Sébastien Lucas <sebastien@slucas.fr>
2016-10-30 04:35:43 +0100

    Add a visual icon to show if a sensor has no recent reading (less than 31 minutes old) available (updated every minute)
    Could be made better with js (animation)

--- app/index/recentData.component.js
+++ app/index/recentData.component.js
@@ -1,8 +1,16 @@
 ﻿'use strict';
 
-function RecentDataCtrl(firebaseHelperService) {
+function RecentDataCtrl(firebaseHelperService, $timeout) {
   var ctrl = this;
   ctrl.readings = {};
+  ctrl.currentTimeLimit = 0;
+
+  ctrl.updateTimestamp = function() {
+    ctrl.currentTimeLimit = Math.floor(Date.now() / 1000) - (31 * 60);
+    $timeout(ctrl.updateTimestamp, 1 * 60 * 1000);
+  };
+
+  ctrl.updateTimestamp();
 
   angular.forEach(ctrl.rooms, function(room) {
     ctrl.readings[room.$id] = firebaseHelperService.getLastReading(room.$id, 1);
