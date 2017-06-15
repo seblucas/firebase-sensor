75d575a2ba8ac8fe3cfb7171820199c9f4f717e1
Sébastien Lucas <sebastien@slucas.fr>
2017-06-15 12:26:35 +0200

    Use proper promises and test all sensors.
    The main problem is that if a sensor is empty it will timeout ....

--- functions/index.js
+++ functions/index.js
@@ -14,17 +14,30 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 exports.monitor = functions.https.onRequest((req, res) => {
   // Push the new message into the Realtime Database using the Firebase Admin SDK.
   const tstamp = Math.floor(Date.now() / 1000);
-  admin.database().ref('/readings/consigne').limitToLast(1).once("child_added", snapshot => {
+  var sensors = ["consigne", "grenier", "cuisine", "deulemont", "palier"];
+  var promiseArray = [];
+  var result = {};
+  for (var i = 0; i < sensors.length; i++) {
+    console.log('Adding promise for ' + sensors[i]);
+    promiseArray.push(admin.database().ref('/readings/' + sensors[i]).limitToLast(1).once("child_added"));
+  }
+  Promise.all(promiseArray).then(snapshots => {
+    console.log('All promises done : ' + snapshots.length);
     res.set('Cache-Control', 'private, max-age=300');
-    differenceInMinutes = (tstamp - snapshot.val().time) / 60;
-    result = {current: tstamp,
-              sensor: snapshot.val().time,
-              diff: Math.round(differenceInMinutes * 10) / 10};
-    if (differenceInMinutes < 31) {
-      result['status'] = "OK";
-    } else {
-      result['status'] = "KO";
+    for (var i = 0; i < snapshots.length; i++) {
+      differenceInMinutes = (tstamp - snapshots[i].val().time) / 60;
+      result[sensors[i]] = {current: tstamp,
+                            sensor: snapshots[i].val().time,
+                            diff: Math.round(differenceInMinutes * 10) / 10};
+      if (differenceInMinutes < 31) {
+        result[sensors[i]]['status'] = "OK";
+      } else {
+        result[sensors[i]]['status'] = "KO";
+      }
     }
     return res.status(200).json(result);
+  }).catch(error => {
+    console.log('Error getting message details', error.message);
+    res.sendStatus(500);
   });
 });
