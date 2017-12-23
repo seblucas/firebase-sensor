8a2a2311bfd59c139192484917618526ac11cf4d
Sébastien Lucas <sebastien@slucas.fr>
2017-12-23 14:28:47 +0100

    Use the html5 history mode for the routing

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import CustomChartPage from '@/components/custom-chart/custom-chart-page'
 Vue.use(Router)
 
 export default new Router({
-  // mode: 'history',
+  mode: 'history',
   routes: [
     {
       path: '/',
