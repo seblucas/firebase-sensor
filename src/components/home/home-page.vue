01939d5e988ce5f397095e2fd82d2e36d48dde87
Sébastien Lucas <sebastien@slucas.fr>
2017-12-23 14:40:55 +0100

    Create a component for the chart summary

--- src/components/home/home-page.vue
+++ src/components/home/home-page.vue
@@ -1,62 +1,24 @@
 <template>
   <div>
     <recent-data :rooms="rooms" :categories="categories"></recent-data>
-    <div class="row" v-for="category in categories">
-      <div class="col-lg-12">
-        <chart-detail v-if="isReadyChart" :rooms="rooms" :category="category" :readings="data"></chart-detail>
-      </div>
-    </div>
+    <chart-summary :rooms="rooms" :categories="categories"></chart-summary>
   </div>
 </template>
 
 <script>
 import RecentData from '@/components/home/recent-data'
-import ChartDetail from '@/components/home/chart-detail'
+import ChartSummary from '@/components/home/chart-summary'
 
 export default {
   name: 'home-page',
-  data () {
-    return {
-      data: {},
-      chartReadyCount: 0,
-      numberOfHours: 48
-    }
-  },
   computed: {
     rooms () {
       return this.$store.getters.rooms
     },
     categories () {
       return this.$store.getters.categoriesSorted
-    },
-    isReadyChart () {
-      return this.chartReadyCount === Object.keys(this.rooms).length
     }
   },
-  methods: {
-    loadData () {
-      this.data = {}
-      this.chartReadyCount = 0
-      var lowerTimeLimit = (new Date() / 1000) - 3600 * this.numberOfHours
-      Object.keys(this.rooms).forEach((roomId) => {
-        console.log('load readings data for room:', roomId)
-        this.$firebase.database().ref('readings/' + roomId).limitToLast(4 * this.numberOfHours).once('value', (newValue) => {
-          console.log(`found readings for ${roomId}`)
-
-          var basicArray = this.ObjectToArray(newValue.val())
-          // Remove too old readings
-          basicArray = basicArray.filter((item) => {
-            return item.time > lowerTimeLimit
-          })
-          this.data[roomId] = basicArray
-          this.chartReadyCount++
-        })
-      })
-    }
-  },
-  created () {
-    this.loadData()
-  },
-  components: {RecentData: RecentData, ChartDetail: ChartDetail}
+  components: {RecentData: RecentData, ChartSummary: ChartSummary}
 }
 </script>
