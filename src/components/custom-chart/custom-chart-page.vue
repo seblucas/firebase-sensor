87577b040c5a10d2981fb5c052cc608a98aace08
Sébastien Lucas <sebastien@slucas.fr>
2018-03-25 09:28:57 +0200

    Fix eslint warnings

--- src/components/custom-chart/custom-chart-page.vue
+++ src/components/custom-chart/custom-chart-page.vue
@@ -4,11 +4,11 @@
       <div class="col-xs-12 col-sm-6 col-md-6">
         <select class="form-control" v-model="selectedCategory">
           <!-- inline object literal -->
-          <option v-for="category in categories" v-bind:value="category">{{ category.label }}</option>
+          <option v-for="category in categories" v-bind:value="category" :key="category.id">{{ category.label }}</option>
         </select>
         <select class="form-control" v-model="selectedSize">
           <!-- inline object literal -->
-          <option v-for="size in chartAllSizes" v-bind:value="size.id">{{ size.value }}</option>
+          <option v-for="size in chartAllSizes" v-bind:value="size.id" :key="size.id">{{ size.value }}</option>
         </select>
       </div>
     </div>
@@ -29,9 +29,10 @@ export default {
     return {
       selectedCategory: null,
       selectedSize: 24,
-      chartAllSizes: [{id: '24', value: '24 hours'},
-                      {id: '48', value: '48 hours'},
-                      {id: '168', value: '1 week'}]
+      chartAllSizes: [
+        {id: '24', value: '24 hours'},
+        {id: '48', value: '48 hours'},
+        {id: '168', value: '1 week'}]
     }
   },
   computed: {
