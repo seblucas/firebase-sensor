08ef29c7e446e46637d954743452c1205b37c52d
Sébastien Lucas <sebastien@slucas.fr>
2018-04-05 13:46:27 +0200

    To allow for unit tested, separate the chart creation from addGraph part from nvd3
    Don't use addGraph for now

--- src/components/home/chart-detail.vue
+++ src/components/home/chart-detail.vue
@@ -66,49 +66,51 @@ export default {
         }
       })
     },
+    generateChart () {
+      this.chart = nv.models.lineChart()
+        .margin({left: 70})
+        .useInteractiveGuideline(true)
+        // .transitionDuration(350)  //how fast do you want the lines to transition?
+        .showLegend(true)
+        .xScale(d3.time.scale())
+        .interpolate('monotone')
+        .x((entry) => { return entry.time })
+        .y((entry) => { return entry[this.category.id] })
+        .forceY([this.category.forceMin, this.category.forceMax])
+        .showYAxis(true)
+        .showXAxis(true)
+
+      var tickMultiFormat = d3.time.format.multi([
+        ['%H:%M', function (d) { return d.getMinutes() }], // not the beginning of the hour
+        ['%a %H:00', function (d) { return d.getHours() }], // not midnight
+        ['%b %-d', function (d) { return d.getDate() !== 1 }], // not the first of the month
+        ['%b %-d', function (d) { return d.getMonth() }], // not Jan 1st
+        ['%Y', function () { return true }]
+      ])
+      this.chart.xAxis
+        .showMaxMin(false)
+        .tickFormat(function (d) { return tickMultiFormat(new Date(d * 1000)) })
+
+      this.chart.yAxis
+        .axisLabel(`${this.category.label} (${this.category.unit})`)
+        .tickFormat(d3.format('.02f'))
+
+      d3.select('#' + this.category.id + ' svg')
+        .datum(this.data)
+        .call(this.chart)
+
+      nv.utils.windowResize(() => { this.chart.update() })
+      return this.chart
+    },
     prepareChart () {
       nv.addGraph(() => {
-        this.chart = nv.models.lineChart()
-          .margin({left: 70})
-          .useInteractiveGuideline(true)
-          // .transitionDuration(350)  //how fast do you want the lines to transition?
-          .showLegend(true)
-          .xScale(d3.time.scale())
-          .interpolate('monotone')
-          .x((entry) => { return entry.time })
-          .y((entry) => { return entry[this.category.id] })
-          .forceY([this.category.forceMin, this.category.forceMax])
-          .showYAxis(true)
-          .showXAxis(true)
-
-        var tickMultiFormat = d3.time.format.multi([
-          ['%H:%M', function (d) { return d.getMinutes() }], // not the beginning of the hour
-          ['%a %H:00', function (d) { return d.getHours() }], // not midnight
-          ['%b %-d', function (d) { return d.getDate() !== 1 }], // not the first of the month
-          ['%b %-d', function (d) { return d.getMonth() }], // not Jan 1st
-          ['%Y', function () { return true }]
-        ])
-        this.chart.xAxis
-          .showMaxMin(false)
-          .tickFormat(function (d) { return tickMultiFormat(new Date(d * 1000)) })
-
-        this.chart.yAxis
-          .axisLabel(`${this.category.label} (${this.category.unit})`)
-          .tickFormat(d3.format('.02f'))
-
-        d3.select('#' + this.category.id + ' svg')
-          .datum(this.data)
-          .call(this.chart)
-
-        nv.utils.windowResize(() => { this.chart.update() })
-        return this.chart
+        return this.generateChart()
       })
     }
   },
   mounted () {
-    this.prepareChart()
-
     this.loadData()
+    this.generateChart()
   }
 }
 </script>
