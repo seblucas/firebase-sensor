8edbe404ad84e5884bb5c79b2d148da73eaf4282
Sébastien Lucas <sebastien@slucas.fr>
2018-04-08 15:54:42 +0200

    [test] Add a proper snpashot test for RecentDataPanel.
    Should check most stuff

--- test/unit/specs/components/home/recent-data-panel.spec.js
+++ test/unit/specs/components/home/recent-data-panel.spec.js
@@ -1,16 +1,65 @@
-import { shallow } from '@vue/test-utils'
+import { shallow, createLocalVue } from '@vue/test-utils'
 import RecentDataPanel from '@/components/home/recent-data-panel'
 import { $firebase, firebaseResult } from '../../../mocks/firebase.mock'
 
 const propsData = {
   room: {
-    label: 'room1',
-    icon: 'icon1',
-    color: 'red1'
-  }
+    color: '#FFCCBC',
+    icon: 'glyphicon-bed',
+    label: 'Room One',
+    readings: {
+      other: 0,
+      temp: 1,
+      hum: 1
+    }
+  },
+  idRoom: 'room1',
+  categories: [
+    {
+      id: 'temp',
+      forceMax: 15,
+      forceMin: 0,
+      fractionSize: 1,
+      label: 'Temperature',
+      order: 1,
+      unit: '°C'
+    },
+    {
+      id: 'hum',
+      forceMax: 80,
+      forceMin: 40,
+      fractionSize: 0,
+      label: 'Humidity',
+      order: 2,
+      unit: '%'
+    }
+  ],
+  timeLimit: 1514764800
 }
 
 describe('RecentDataPanel.vue', () => {
+  it('shows a panel with many values', () => {
+    firebaseResult.mockReset()
+    firebaseResult.mockReturnValue({ id: 'L00', time: 1514764800, temp: 12.1, hum: 52, lum: 17 })
+
+    const localVue = createLocalVue()
+    localVue.filter('formatDate', (value) => {
+      if (value) {
+        return new Date(value * 1000).toLocaleString()
+      }
+    })
+    localVue.filter('formatNumber', (value, fractionSize) => {
+      if (value) {
+        return value.toLocaleString(undefined, { minimumFractionDigits: fractionSize, maximumFractionDigits: fractionSize })
+      }
+    })
+    const wrapper = shallow(RecentDataPanel, {
+      localVue,
+      propsData,
+      mocks: { $firebase }
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
   it('shows the room label passed in props', () => {
     firebaseResult.mockReset()
     firebaseResult.mockReturnValue(false)
@@ -20,6 +69,6 @@ describe('RecentDataPanel.vue', () => {
       mocks: { $firebase }
     })
     const h2 = wrapper.find('.panel-footer > p')
-    expect(h2.text()).toBe('room1')
+    expect(h2.text()).toBe('Room One')
   })
 })
