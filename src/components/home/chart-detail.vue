ea7d2dea42e712a34b3c7143e7326d18c13522ff
Sébastien Lucas <sebastien@slucas.fr>
2018-04-15 11:07:31 +0200

    [style] Fix the coding style (space before parenthesis)

--- src/components/home/chart-detail.vue
+++ src/components/home/chart-detail.vue
@@ -105,7 +105,7 @@ export default {
         return this.generateChart()
       })
     },
-    loadDataAndGraph() {
+    loadDataAndGraph () {
       this.loadData()
       this.generateChart()
     }
