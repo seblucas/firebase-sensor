d3e8b1077b1a5855c78d7a912d78674df1b284fe
Sébastien Lucas <sebastien@slucas.fr>
2018-12-24 15:09:21 +0100

    [fix] Enhance statictics handling

--- src/components/statistics/statistics-page.vue
+++ src/components/statistics/statistics-page.vue
@@ -25,8 +25,9 @@ export default {
   name: 'statistics-page',
   data () {
     return {
+      year: 2016,
       start: 0,
-      max: 180,
+      max: 31,
       elapsed: 0,
       stats: [],
       hashStats: {}
@@ -42,6 +43,9 @@ export default {
   },
   methods: {
     normalizeFloat (maybeString) {
+      if (maybeString === undefined || maybeString === null) {
+        return null
+      }
       if (typeof (maybeString) === 'string') {
         return parseFloat(maybeString)
       }
@@ -49,13 +53,17 @@ export default {
     },
     async calc3 () {
       this.hashStats = {}
-      let timeStart = new Date(2015, 10, 1, 0, 0, 1).getTime() / 1000
+      let timeStart = new Date(this.year, 0, 1, 0, 0, 0).getTime() / 1000
       // const categories = [ 'temp', 'hum' ]
       this.start = 0
+      this.elapsed = 0
       const t0 = performance.now()
       while (this.start < this.max) {
         const timeEnd = timeStart + 86399
         for (let room of this.rooms) {
+          if (room.id === 'consigne') {
+            continue
+          }
           let ref = this.$firebase.database().ref('readings/' + room.id).orderByChild('time')
           ref = ref.startAt(timeStart)
           ref = ref.endAt(timeEnd)
@@ -66,29 +74,35 @@ export default {
             var basicArray = this.ObjectToArray(data.val())
 
             if (basicArray.length > 64) {
-              if (!this.hashStats.hasOwnProperty(key)) {
-                this.hashStats[key] = {
-                  time: timeStart + 43200
-                }
+              // if (!this.hashStats.hasOwnProperty(key)) {
+              this.hashStats[key] = {
+                time: timeStart + 43200
               }
+              // }
               this.categories.forEach(category => {
                 const cat = category.id
                 if (!room.readings.hasOwnProperty(cat) ||
                   room.readings[cat] === 0) {
                   return
                 }
-                const dataCat = basicArray.map(i => this.normalizeFloat(i[cat]))
+                const dataCat = basicArray.filter(i => i.hasOwnProperty(cat)).map(i => this.normalizeFloat(i[cat]))
+                if (dataCat.length < 65) {
+                  return
+                }
 
                 this.hashStats[key][cat] = dataCat.reduce((s, i) => s + i, 0) / dataCat.length
                 this.hashStats[key][cat + '_min'] = Math.min(...dataCat)
                 this.hashStats[key][cat + '_max_'] = Math.max(...dataCat)
                 this.hashStats[key][cat + '_count'] = dataCat.length
               })
+              const newItem = await this.$firebase.database().ref('stats_daily/' + room.id + '/' + this.year.toString()).push(this.hashStats[key])
+              console.log('New item added ', newItem.key)
             }
           } else {
             console.log('nothing found for ', key)
           }
         }
+        this.elapsed = performance.now() - t0
         timeStart += 86400
         this.start++
       }
