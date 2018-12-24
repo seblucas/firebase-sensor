3aec6f2f63fc4488887686125248582b9131139b
Sébastien Lucas <sebastien@slucas.fr>
2018-12-24 15:57:33 +0100

    [fix] Refactor the statistics to prepare for monthly statistics

--- src/components/statistics/statistics-page.vue
+++ src/components/statistics/statistics-page.vue
@@ -42,6 +42,7 @@ export default {
     }
   },
   methods: {
+
     normalizeFloat (maybeString) {
       if (maybeString === undefined || maybeString === null) {
         return null
@@ -51,10 +52,46 @@ export default {
       }
       return maybeString
     },
+    calcDailyStats (room, basicArray, dataObject) {
+      this.categories.forEach(category => {
+        const cat = category.id
+        if (!room.readings.hasOwnProperty(cat) ||
+          room.readings[cat] === 0) {
+          return
+        }
+        const dataCat = basicArray.filter(i => i.hasOwnProperty(cat)).map(i => this.normalizeFloat(i[cat]))
+        if (dataCat.length < 65) {
+          return
+        }
+
+        dataObject[cat] = dataCat.reduce((s, i) => s + i, 0) / dataCat.length
+        dataObject[cat + '_min'] = Math.min(...dataCat)
+        dataObject[cat + '_max_'] = Math.max(...dataCat)
+        dataObject[cat + '_count'] = dataCat.length
+      })
+    },
+    processMonthlyStats () {
+
+    },
     async calc3 () {
       this.hashStats = {}
+      for (let room of this.rooms) {
+        this.hashStats[room.id] = {
+          '01': [],
+          '02': [],
+          '03': [],
+          '04': [],
+          '05': [],
+          '06': [],
+          '07': [],
+          '08': [],
+          '09': [],
+          '10': [],
+          '11': [],
+          '12': []
+        }
+      }
       let timeStart = new Date(this.year, 0, 1, 0, 0, 0).getTime() / 1000
-      // const categories = [ 'temp', 'hum' ]
       this.start = 0
       this.elapsed = 0
       const t0 = performance.now()
@@ -68,34 +105,18 @@ export default {
           ref = ref.startAt(timeStart)
           ref = ref.endAt(timeEnd)
           const data = await ref.once('value')
-          const key = room.id + '_' + new Date((timeStart + 43200) * 1000).toISOString().substring(0, 10)
+          const key = room.id + '-' + new Date((timeStart + 43200) * 1000).toISOString().substring(0, 10)
           if (data.exists()) {
-            // console.log(data.val())
             var basicArray = this.ObjectToArray(data.val())
-
+            var keyElements = key.split('-')
+            console.log('elem', keyElements)
+            const dataObject = {
+              time: timeStart + 43200
+            }
+            this.calcDailyStats(room, basicArray, dataObject)
+            this.hashStats[keyElements[0]][keyElements[2]].push(dataObject)
             if (basicArray.length > 64) {
-              // if (!this.hashStats.hasOwnProperty(key)) {
-              this.hashStats[key] = {
-                time: timeStart + 43200
-              }
-              // }
-              this.categories.forEach(category => {
-                const cat = category.id
-                if (!room.readings.hasOwnProperty(cat) ||
-                  room.readings[cat] === 0) {
-                  return
-                }
-                const dataCat = basicArray.filter(i => i.hasOwnProperty(cat)).map(i => this.normalizeFloat(i[cat]))
-                if (dataCat.length < 65) {
-                  return
-                }
-
-                this.hashStats[key][cat] = dataCat.reduce((s, i) => s + i, 0) / dataCat.length
-                this.hashStats[key][cat + '_min'] = Math.min(...dataCat)
-                this.hashStats[key][cat + '_max_'] = Math.max(...dataCat)
-                this.hashStats[key][cat + '_count'] = dataCat.length
-              })
-              const newItem = await this.$firebase.database().ref('stats_daily/' + room.id + '/' + this.year.toString()).push(this.hashStats[key])
+              const newItem = await this.$firebase.database().ref('stats_daily/' + this.year.toString() + '/' + room.id).push(dataObject)
               console.log('New item added ', newItem.key)
             }
           } else {
